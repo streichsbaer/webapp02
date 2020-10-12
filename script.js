9d36b7a10b8b2a7b168fd6c4946df2092effe5f3
LegCow <36110770+legcow02@users.noreply.github.com>
2020-10-12 11:22:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
    String addr = request.getParameter("addr");
    String[] ping = {"/bin/sh", "-c", "ping -c2 " + addr};
    String output = "";
-   String password = "password";
+   String password = "Qd9ttpTc6@HUU]f[";
+   String pass = "my_static_passcode";
    if(ping != null) {
       String s = null;
       try {
